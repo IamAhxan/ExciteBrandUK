27b27794dae9c20269496ac2eb305c621cedbc0e
IamAhxan <mohammadahsan7744@gmail.com>
2021-08-11 20:15:12 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+(function ($) {
+    // Testimonials carousel (uses the Owl Carousel library)
+    $(".testimonials-carousel").owlCarousel({
+        autoplay: true,
+        loop: true,
+        items: 1
+    });
+
+})(jQuery);
+
